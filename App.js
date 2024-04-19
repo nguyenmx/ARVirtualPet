1bfbd2cb19c1e08770c6f29a7cc7c890ba302798
Mary Nguyen <mary.nguyenwa18@gmail.com>
2024-04-19 16:16:31 -0700

    Added basic home screen and links to the camera

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ARCamera from './screens/ARCamera';
+
+const Stack = createStackNavigator();
+
+function HomeScreen() {
+  const navigation = useNavigation();
+  
+  const goTo3DModelScreen = () => {
+    navigation.navigate('Display3DModel');
+  };
+
+  const goToARCameraScreen = () => {
+    navigation.navigate('ARCamera');
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={goTo3DModelScreen}>
+        <Text style={styles.buttonText}>3D Model</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={goToARCameraScreen}>
+        <Text style={styles.buttonText}>AR Camera</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerTransparent: true, title: '' }}
+        />
+        {/* <Stack.Screen name="Display3DModel" component={Display3DModel} options={{ headerShown: false }}/> */}
+        <Stack.Screen name="ARCamera" component={ARCamera} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    width: 200,
+    height: 50,
+    backgroundColor: 'orange',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
   },
 });
